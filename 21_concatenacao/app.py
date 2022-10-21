c03391dfb9ce991124ab5e51a064497ea1ca5c9a
Gliverson-Almeida <gliversonfrp@gmail.com>
2022-10-21 11:35:46 -0300

    soma string

---
+++ 21_concatenacao/app.py
@@ -0,0 +1,14 @@
+nome = "Gliverson"
+
+print("Olá "+ nome)
+
+velocidade = "200"
+carro = "Ferrari"
+cidade = "Curitiba"
+
+print("O veículo " + carro + " estava a " + velocidade + "km/h em " + cidade)
+
+saudacao = "Olá"
+
+print(saudacao + " " + nome)
+ 
